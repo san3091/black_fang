180968487847a9532f4629051c7de6a606e2ba61
san3091 <san3091@gmail.com>
2017-05-20 23:19:30 -0400

    ADDED DB seed data
    ADD v1 seeds before discovering studff about users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+ApplicationRecord.transaction do
+  puts "starting db seed"
+
+  mike = User.create! email: Faker::Internet.email, 
+                      first_name: "Mike",
+                      last_name: "Gergory"
+
+  dance = Dance.create! title: Faker::Food.ingredient,
+                        description: Faker::Food.measurement
+
+  video = Video.new youtube_url: Faker::Internet.url,
+                        title: Faker::Zelda.game
+
+  parent_video = Video.new youtube_url: Faker::Internet.url,
+                               title: Faker::Zelda.game
+
+  child_video = Video.new youtube_url: Faker::Internet.url,
+                              title: Faker::Zelda.game
+
+  mike.dances << dance
+  puts "added dance #{dance.title} to Mike"
+  dance.videos << video
+  puts "added video #{video.title} to dance #{dance.title}"
+  video.parents << parent_video
+  puts "added parent #{parent_video.title} to dance #{dance.title}"
+  video.children << child_video
+  puts "added child #{child_video} to dance #{dance.title}"
+  video.save!
+  parent_video.save!
+  child_video.save!
+
+  puts "end db seed"
+end
